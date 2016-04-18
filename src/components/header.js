9f60c963ee655e49c7778fc90bdf8802b9cce171
glundgren93 <glundgren93@gmail.com>
2016-04-18 16:28:20 -0300

    change header component to functional component

--- src/components/header.js
+++ src/components/header.js
@@ -2,14 +2,14 @@
   Component responsible for showing app header.
 */
 
-import React, { Component } from 'react';
+import React from 'react';
 
-export default class Header extends Component{
-  render() {
-    return (
-      <div className="page-header">
-        <h1>TV Show React <small>Made for study purposes</small></h1>
-      </div>
-    );
-  }
+const Header = () => {
+  return (
+    <div className="page-header">
+      <h1>TV Show React <small>Made for study purposes</small></h1>
+    </div>
+  );
 }
+
+export default Header;
