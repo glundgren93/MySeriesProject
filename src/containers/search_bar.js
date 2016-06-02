1c36c48bbc1612911a426a4d81be5e41d8e84b55
Gabriel Leitão Lundgren <gabriel.lundgren@eldorado.org.br>
2016-06-02 16:50:23 -0300

    removed bindactioncreators

--- src/containers/search_bar.js
+++ src/containers/search_bar.js
@@ -4,7 +4,6 @@
 
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import { bindActionCreators } from 'redux';
 import { fetchShows } from '../actions/index';
 
 class SearchBar extends Component {
--- src/containers/shows_list.js
+++ src/containers/shows_list.js
@@ -4,7 +4,6 @@
 
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import { bindActionCreators } from 'redux';
 import { selectShow } from '../actions/index';
 import { Link } from 'react-router';
 
